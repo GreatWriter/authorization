026771279207a2dcec3f59f37f08dde58a2027c5
ppx_let's_go <ppx_let's_go@users.noreply.github.com>
2017-12-22 17:10:52 +0800

    Initial commit

---
+++ src/main/java/com/example/authorization/web/TestController.java
@@ -0,0 +1,33 @@
+package com.example.authorization.web;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/test/{id}")
+    public String test(@PathVariable String id){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        return "PathVariable:" + id;
+    }
+
+    @GetMapping("/product/{id}")
+    public String getProduct(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "product id : " + id;
+    }
+
+    @GetMapping("/order/{id}")
+    public String getOrder(@PathVariable String id) {
+        //for debug
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return "order id : " + id;
+    }
+}
